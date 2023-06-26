d13538ea40495b18855ef84623a874b5b07e2fa5
Sitran ÇELİK <sitran.celik@gmail.com>
2023-06-26 18:31:16 +0300

    YOL AYRIMI UYGULAMASI
    Else if yapısını açık bir şekilde anlamayı kolaylaştıran başlangıç uygulaması

---
+++ app.js
@@ -0,0 +1,14 @@
+let secilenYol = prompt("Lütfen gitmek istediğiniz yolu seçiniz:");
+
+if (secilenYol == 1) {
+    alert("seçilen yol " + secilenYol + ".yoldur.");
+}
+else if (secilenYol == 2) {
+    alert("seçilen yol " + secilenYol + ".yoldur.");
+}
+else if (secilenYol == 3) {
+    alert("seçilen yol " + secilenYol + ".yoldur.");
+}
+else {
+    alert("Lütfen geçerli bir yol seçiniz")
+}
